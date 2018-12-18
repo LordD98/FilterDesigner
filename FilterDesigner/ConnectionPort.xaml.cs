ceacab891d5918a20aff1acf0767190310e5c224
LordD98 <lorddelhey@gmail.com>
2018-12-19 00:02:36 +0100

    -Added Graphical Interface -Path Search working (Prototype)

---
+++ FilterDesigner/ConnectionPort.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace FilterDesigner
+{
+	/// <summary>
+	/// Interaktionslogik für ConnectionPort.xaml
+	/// </summary>
+	public partial class ConnectionPort : UserControl
+	{
+		public static Brush SelectionColorBrush = new SolidColorBrush(Color.FromArgb(0xFF,0xFF,0x20,0x20));
+
+		public ConnectionPort()
+		{
+			InitializeComponent();
+		}
+
+		private void Ellipse_Enter(object sender, MouseEventArgs e)
+		{
+			(sender as Ellipse).Stroke = SelectionColorBrush;
+		}
+
+		private void Ellipse_Leave(object sender, MouseEventArgs e)
+		{
+			(sender as Ellipse).Stroke = Brushes.Transparent;
+		}
+	}
+}
--- FilterDesigner/MainWindow.xaml.cs
+++ FilterDesigner/MainWindow.xaml.cs
@@ -1,40 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FilterDesigner
 {
-	/// <summary>
-	/// Interaktionslogik für MainWindow.xaml
-	/// </summary>
+	// TODO:
+	// Rotate Components, Display Names
+	// Connect
+
 	public partial class MainWindow : Window
 	{
+		public static List<Component> allComponents;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			Component.baseCanvas = canvas;
+			canvas.MouseLeftButtonUp += Component.Symbol_MouseUp;
+			canvas.MouseMove += Component.Symbol_MouseMove;
+			allComponents = new List<Component>();
 
 			Net GND = new Net("GND");
 			Net U1 = new Net("U1");
 			Net U2 = new Net("U2");
+			Net U_dash = new Net("U'");
 
-			Resistor R1 = new Resistor("R1");
-			Capacitor C1 = new Capacitor("C1");
+			Resistor R1 = new Resistor("R1", 100, 100);
+			Resistor R2 = new Resistor("R2", 200, 100);
+			Resistor R_dash = new Resistor("R'", 150, 200);
+			Capacitor C1 = new Capacitor("C1", 300, 150);
+			Inductor I = new Inductor("L", 200, 300);
 
-			U1.Connect(U2, R1);
-			U2.Connect(GND, C1);
+			R1.Connect(U1, U_dash);
+			R2.Connect(U_dash, U2);
+			R_dash.Connect(U2, U1);
+			C1.Connect(GND, U2);
+			
+			DrawAll();
+			List<Path> paths = FindPaths(U1, GND);
+		}
 
-			List<Path> paths = FindPaths(U1, U2);
+		public void DrawAll()
+		{
+			foreach(Component comp in allComponents)
+			{
+				comp.Draw();
+			}
 		}
 
 		public List<Path> FindPaths(Net A, Net B)
@@ -46,51 +61,58 @@ namespace FilterDesigner
 			//List<Net> visitedNets = new List<Net>();    // Nets that are completed
 			Path currentPath = new Path();
 			List<Net> currentNetOrder = new List<Net>();    // Represents the current parse state 
-			List<Component> usedComponents = new List<Component>();
+			currentNetOrder.Add(A);
+			//List<Component> usedComponents = new List<Component>();
 			Component currentComp = null;
 			while(!done)
 			{
 				currentComp = currentNet.GetNextComponent(currentComp);
 				if(currentComp == null)
 				{
-					currentPath.path.Remove(currentComp);
 					currentNetOrder.Remove(currentNet);     // maybe combine these? (currentNet always
 					if(currentNetOrder.Count == 0)
 					{
 						done = true;
 					}
-					currentNet = currentNetOrder.Last();	//				of currentNetOrder?)
+					else
+					{
+						currentComp = currentPath.path.Last();
+						currentPath.path.Remove(currentComp);
+						currentNet = currentNetOrder.Last();	//				of currentNetOrder?)
+
+					}
 					continue;
 				}
 
-				if(usedComponents.Contains(currentComp))
+				if(currentPath.path.Contains(currentComp))
 				{
 					continue;
 				}
 				else
 				{
-					usedComponents.Add(currentComp);
-					currentPath.Add(currentComp);
-					currentNetOrder.Add(currentComp.OtherNet(currentNet));
+					if(currentComp.IsUseless())    // comp. is connected badly
+					{
+						continue;
+					}
+					else if(currentComp.IsConnected(B))
+					{
+						paths.Add(currentPath.Copy().Add(currentComp));
+						continue;
+					}
+					else if(currentNetOrder.Contains(currentComp.OtherNet(currentNet)))
+					{
+						continue;
+					}
+					else
+					{
+						currentNet = currentComp.OtherNet(currentNet);
+						currentNetOrder.Add(currentNet);
+						currentPath.Add(currentComp);
+						currentComp = null;
+					}
 				}
 				
 				//if()
-				if(currentComp.NetA == currentNet && currentComp.NetB == currentNet)
-				{
-					continue;
-				}
-				else if(currentComp.IsConnected(B))
-				{
-					paths.Add(currentPath.Copy());
-					currentPath.path.Remove(currentComp);
-					currentNetOrder.Remove(currentNet);
-					currentNet = currentComp.OtherNet(currentNet);
-					continue;
-				}
-				else if(!currentNetOrder.Contains(currentComp.OtherNet(currentComp.NetA)))
-				{
-					currentNetOrder.Add(currentComp.OtherNet(currentComp.NetA));
-				}
 			}
 
 			return paths;
@@ -106,9 +128,10 @@ namespace FilterDesigner
 			path = new List<Component>();
 		}
 
-		public void Add(Component component)
+		public Path Add(Component component)
 		{
 			path.Add(component);
+			return this;
 		}
 
 		public Path Copy()
@@ -147,12 +170,12 @@ namespace FilterDesigner
 			if(components.Contains(component))
 			{
 				int index = components.IndexOf(component);
-				if(index == components.Count - 1)
+				if(index < components.Count-1)
 				{
 					return components[index + 1];
 				}
 			}
-			return component;   // return null/exception?
+			return null;
 		}
 
 		public void Connect(Net net, Component component)
@@ -164,9 +187,56 @@ namespace FilterDesigner
 		}
 	}
 
+	public enum NetPort { PortA, PortB };
+
 	public abstract class Component
 	{
 		public string Name { get; set; }
+
+		public const int WireThickness = 3;
+		
+		private double x;
+		public double X
+		{
+			get
+			{
+				return x;
+			}
+			set
+			{
+				x = value;
+				Canvas.SetLeft(VisualGroup, x);
+			}
+		}
+
+		private double y;
+		public double Y
+		{
+			get
+			{
+				return y;
+			}
+			set
+			{
+				y = value;
+				Canvas.SetTop(VisualGroup, y);
+			}
+		}
+
+		private static bool wireAttached = false;
+		private static Component attachedComponent;
+		private static NetPort attachedPort;
+
+		private static double oldX;
+		private static double oldY;
+		private static double mousedown_x = -1;
+		private static double mousedown_y = -1;
+		private static Component movedComponent;
+
+		public static Canvas baseCanvas;
+
+		public Canvas VisualGroup;
+
 		public Net NetA { get; set; }
 		public Net NetB { get; set; }
 		public Impedance Impedance { get; }
@@ -174,6 +244,21 @@ namespace FilterDesigner
 		protected Component(string name)
 		{
 			Name = name;
+			VisualGroup = new Canvas
+			{
+				Background = Brushes.Transparent,  // Transparent
+				Width = 90,
+				Height = 20
+			};
+			VisualGroup.MouseLeftButtonDown += (x, y) => Symbol_MouseDown(this, y);
+			Panel.SetZIndex(VisualGroup, 0);
+			MainWindow.allComponents.Add(this);
+		}
+
+		protected Component(string name, int x, int y) : this(name)
+		{
+			X = x;
+			Y = y;
 		}
 
 		public abstract string GetValueStr();
@@ -188,56 +273,292 @@ namespace FilterDesigner
 			return net == NetA ? NetB : NetA;
 		}
 
+		public void Connect(Net A, Net B)
+		{
+			A.components.Add(this);
+			B.components.Add(this);
+			NetA = A;
+			NetB = B;
+		}
+
+		public bool IsUseless()
+		{
+			if(NetA == null || NetB == null || NetA == NetB)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public abstract void Draw();
+
+		protected static void Symbol_MouseDown(Component sender, MouseButtonEventArgs e)
+		{
+			int oldZIndex = Panel.GetZIndex(sender.VisualGroup);
+			foreach(Canvas childCanvas in baseCanvas.Children)
+			{
+				int currentZIndex = Panel.GetZIndex(childCanvas);
+				if(currentZIndex > oldZIndex)
+					Panel.SetZIndex(childCanvas, currentZIndex-1);
+			}
+			Panel.SetZIndex(sender.VisualGroup, baseCanvas.Children.Count-1);
+			sender.VisualGroup.CaptureMouse();
+			movedComponent = sender;
+			Point mousePos = Mouse.GetPosition(baseCanvas);
+			mousedown_x = mousePos.X;
+			mousedown_y = mousePos.Y;
+			oldX = sender.X;
+			oldY = sender.Y;
+		}
+
+		public static void Symbol_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+			if(movedComponent != null)
+			{
+				movedComponent.VisualGroup.ReleaseMouseCapture();
+				movedComponent = null;
+				mousedown_x = -1;
+				mousedown_y = -1;
+			}
+		}
+
+		public static void Symbol_MouseMove(object sender, MouseEventArgs e)
+		{
+			if(Mouse.LeftButton.HasFlag(MouseButtonState.Pressed) && movedComponent != null)
+			{
+				Point newMousePos = Mouse.GetPosition(baseCanvas);
+				movedComponent.X = oldX - (mousedown_x - newMousePos.X);
+				movedComponent.Y = oldY - (mousedown_y - newMousePos.Y);
+			}
+		}
+		
+		public static void ClickConnect(Component sender, NetPort port, MouseButtonEventArgs e)
+		{
+			e.Handled = true;   // Stop event propagation to prevent the Port from hiding
+
+			if(wireAttached)
+			{
+				wireAttached = false;
+				if(attachedComponent == sender && port == attachedPort)
+				{
+					return;
+				}
+				Line
+			}
+			else
+			{
+				attachedComponent = sender;
+				attachedPort = port;
+				wireAttached = true;
+			}
+		}
+
+		public abstract (double X, double Y) GetPortCoordinate(NetPort port);
 	}
 
 	public class Resistor : Component
 	{
 		private double resistance;
 
-		public string Name { get; set; }
-		public Net NetA { get; set; }
-		public Net NetB { get; set; }
-		public Impedance Impedance { get; }
-
 		public Resistor(string name) : base(name) { }
+		public Resistor(string name, int x, int y) : base(name, x, y) { }
 
 		public override string GetValueStr()
 		{
 			return Name;
+		}
+		
+		public override void Draw()
+		{
+			Rectangle symbol = new Rectangle
+			{
+				Width = 50,
+				Height = 20,
+				Fill = Brushes.White,
+				Stroke = Brushes.Black,
+				StrokeThickness = 2,
+				SnapsToDevicePixels = true
+			};
+			Line leads = new Line
+			{
+				X1 = 0,
+				X2 = 90,
+				Y1 = 0,
+				Y2 = 0,
+				Fill = Brushes.Black,   // Dark Gray/Black/Red?
+				Stroke = Brushes.Black,
+				StrokeThickness = WireThickness,
+				SnapsToDevicePixels = true
+			};
+			ConnectionPort portA = new ConnectionPort();
+			ConnectionPort portB = new ConnectionPort();
+
+			portA.MouseLeftButtonDown += (x, y) => ClickConnect(this, NetPort.PortA, y);
+			portB.MouseLeftButtonDown += (x, y) => ClickConnect(this, NetPort.PortB, y);
+			 
+			Canvas.SetLeft(VisualGroup, X);
+			Canvas.SetTop(VisualGroup, Y);
+			Canvas.SetLeft(symbol, 20);
+			Canvas.SetTop(symbol, 0);
+			Canvas.SetLeft(leads, 0);
+			Canvas.SetTop(leads, 10);
+			Canvas.SetLeft(portA, -5);
+			Canvas.SetLeft(portB, 85);
+			Canvas.SetTop(portA, 5);
+			Canvas.SetTop(portB, 5);
+			VisualGroup.Children.Add(leads);
+			VisualGroup.Children.Add(symbol);
+			VisualGroup.Children.Add(portA);
+			VisualGroup.Children.Add(portB);
+			baseCanvas.Children.Add(VisualGroup);
+		}
+
+		public override (double X, double Y) GetPortCoordinate(NetPort port)
+		{
+			if(port == NetPort.PortA)
+			{
+				return (0, 0); 
+			}
+			else
+			{
+				return (0, 0);
+			}
 		}
 	}
 
 	public class Inductor : Component
 	{
 		private double inductance;
-
-		public string Name { get; set; }
-		public Net NetA { get; set; }
-		public Net NetB { get; set; }
-		public Impedance Impedance { get; }
-
+		
 		public Inductor(string name) : base(name) { }
+		public Inductor(string name, int x, int y) : base(name, x, y) { }
 
 		public override string GetValueStr()
 		{
 			return "s" + Name;
+		}
+
+		public override void Draw()
+		{
+			Rectangle symbol = new Rectangle
+			{
+				Width = 50,
+				Height = 20,
+				Fill = Brushes.Black,
+				Stroke = Brushes.Black,
+				StrokeThickness = 2,
+				SnapsToDevicePixels = true
+			};
+			Line leads = new Line
+			{
+				X1 = 0,
+				X2 = 90,
+				Y1 = 0,
+				Y2 = 0,
+				Fill = Brushes.Black,   // Dark Gray/Black/Red?
+				Stroke = Brushes.Black,
+				StrokeThickness = WireThickness,
+				SnapsToDevicePixels = true
+			};
+			ConnectionPort portA = new ConnectionPort();
+			ConnectionPort portB = new ConnectionPort();
+
+			portA.MouseLeftButtonDown += (x, y) => ClickConnect(this, NetPort.PortA, y);
+			portB.MouseLeftButtonDown += (x, y) => ClickConnect(this, NetPort.PortB, y);
+
+			Canvas.SetLeft(VisualGroup, X);
+			Canvas.SetTop(VisualGroup, Y);
+			Canvas.SetLeft(symbol, 20);
+			Canvas.SetTop(symbol, 0);
+			Canvas.SetLeft(leads, 0);
+			Canvas.SetTop(leads, 10);
+			Canvas.SetLeft(portA, -5);
+			Canvas.SetLeft(portB, 85);
+			Canvas.SetTop(portA, 5);
+			Canvas.SetTop(portB, 5);
+			VisualGroup.Children.Add(leads);
+			VisualGroup.Children.Add(symbol);
+			VisualGroup.Children.Add(portA);
+			VisualGroup.Children.Add(portB);
+			baseCanvas.Children.Add(VisualGroup);
 		}
 	}
 
 	public class Capacitor : Component
 	{
 		private double capacitance;
-
-		public string Name { get; set; }
-		public Net NetA { get; set; }
-		public Net NetB { get; set; }
-		public Impedance Impedance { get; }
-
-		public Capacitor(string name) : base(name) { }
+		
+		public Capacitor(string name) : base(name)
+		{
+			VisualGroup.Width = 30;
+			VisualGroup.Height = 40;
+		}
+		public Capacitor(string name, int x, int y) : base(name, x, y)
+		{
+			VisualGroup.Width = 50;
+			VisualGroup.Height = 40;
+		}
 
 		public override string GetValueStr()
 		{
 			return $"1/(s{Name})";
+		}
+
+		public override void Draw()
+		{
+			Border border = new Border
+			{
+				Width = 10,
+				Height = 40,
+				BorderThickness = new Thickness(3, 0, 2.5, 0),
+				BorderBrush = Brushes.Black,
+				SnapsToDevicePixels = true
+			};
+			Rectangle capContent = new Rectangle
+			{
+				Height = border.Height,
+				Width = border.Width,
+				Fill = Brushes.White,
+				SnapsToDevicePixels = true
+			};
+			Line leads = new Line
+			{
+				X1 = 0,
+				X2 = 50,
+				Y1 = 0,
+				Y2 = 0,
+				Fill = Brushes.Black,   // Dark Gray/Black/Red?
+				Stroke = Brushes.Black,
+				StrokeThickness = WireThickness,
+				SnapsToDevicePixels = true
+			};
+			ConnectionPort portA = new ConnectionPort();
+			ConnectionPort portB = new ConnectionPort();
+			
+			portA.MouseLeftButtonDown += (x, y) => ClickConnect(this, NetPort.PortA, y);
+			portB.MouseLeftButtonDown += (x, y) => ClickConnect(this, NetPort.PortB, y);
+
+			border.Child = capContent;
+			Panel.SetZIndex(border, 1);
+			Panel.SetZIndex(leads, 0);
+			Panel.SetZIndex(capContent, 2);
+			Panel.SetZIndex(portA, 3);
+			Panel.SetZIndex(portB, 4);
+			Canvas.SetLeft(VisualGroup, X);
+			Canvas.SetTop(VisualGroup, Y);
+			Canvas.SetLeft(border, 20);
+			Canvas.SetTop(border, 0);
+			Canvas.SetLeft(leads, 0);
+			Canvas.SetTop(leads, 20);
+			Canvas.SetLeft(portA, -5);
+			Canvas.SetLeft(portB, 45);
+			Canvas.SetTop(portA, 15);
+			Canvas.SetTop(portB, 15);
+			VisualGroup.Children.Add(leads);
+			VisualGroup.Children.Add(border);
+			VisualGroup.Children.Add(portA);
+			VisualGroup.Children.Add(portB);
+			baseCanvas.Children.Add(VisualGroup);
 		}
 	}
 
